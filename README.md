e66f3fd62f6ec68042cd3a3cfa313b7f2fa18b5d
grace-ko <gracekoder@gmail.com>
2017-02-26 21:37:17 -0800

    added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+#Pirate Bartender
+Tell your pirate bartender the preferences for yer drinks to generate its ingredients! 
+(OOP exercise)
+
+![screenshot](screenshot.png)
--- app.js
+++ app.js
@@ -1,56 +1,49 @@
-
-var pirateBartender = [
-  { question: "Do ye like yer drinks strong?",
+var pirateBartender = [{
+    question: "Do ye like yer drinks strong?",
     type: "strong",
     ingredient: ["glug of rum", "slug of whisky", "splash of gin"]
-  },
-  { question: "Do ye like it with a salty tang?",
+}, {
+    question: "Do ye like it with a salty tang?",
     type: "salty",
     ingredient: ["olive on a stick", "salt-dusted rim", "rasher of bacon"]
-    },
-  { question: "Are ye a lubber who likes it bitter?", 
+}, {
+    question: "Are ye a lubber who likes it bitter?",
     type: "bitter",
     ingredient: ["Shake of bitters", "splash of tonic", "twist of lemon peel"]
-  },
-  { question: "Would ye like a bit of sweetness with yer poison?",
+}, {
+    question: "Would ye like a bit of sweetness with yer poison?",
     type: "sweet",
     ingredient: ["sugar cube", "spoonful of honey", "splash of cola"]
-  },
-  { question: "Are ye one for a fruity finish?",
+}, {
+    question: "Are ye one for a fruity finish?",
     type: "fruity",
     ingredient: ["slice of orange", "dash of cassis", "cherry on top"]
-  }
-];
+}];
 
-$(function(){
-  $('.drinksResult').hide();
-  var chooseIngredients = "";
- 
-  for (var i = 0; i < pirateBartender.length; i++) {
-    $('.questions').append('<p>' + pirateBartender[i].question +'</p>' + 
-      "Aye!" + '<input type = "radio" name = "' + pirateBartender[i].type + '" value = "yes">' +
-      "No" + '<input type = "radio" name = "' + pirateBartender[i].type + '" value = "no">' 
-    );
-  }
+$(function() {
+    $('.drinksResult').hide();
+    var chooseIngredients = "";
+
+    for (var i = 0; i < pirateBartender.length; i++) {
+        $('.questions').append('<p>' + pirateBartender[i].question + '</p>' +
+            "Aye!" + '<input type = "radio" name = "' + pirateBartender[i].type + '" value = "yes">' +
+            "No" + '<input type = "radio" name = "' + pirateBartender[i].type + '" value = "no">'
+        );
+    }
     $('button').click(function() {
-      $('.drinksResult').show();
-      $('.questions').hide();
-      $('button').hide();
-      for (var i = 0; i < pirateBartender.length; i++) {
-        if ($('input[name="' + pirateBartender[i].type + '"]:checked').val() == "yes") {
-          chooseIngredients+= '<p>' + pirateBartender[i].ingredient[randomIngredient(2, 0)] + '</p>';  
+        $('.drinksResult').show();
+        $('.questions').hide();
+        $('button').hide();
+        for (var i = 0; i < pirateBartender.length; i++) {
+            if ($('input[name="' + pirateBartender[i].type + '"]:checked').val() == "yes") {
+                chooseIngredients += '<p>' + pirateBartender[i].ingredient[randomIngredient(2, 0)] + '</p>';
+            }
         }
-      }
-      $('.drinksResult').append('<p>' + "The ingredients for yer drinks:" + '</p>' + chooseIngredients);
-    });  
+        $('.drinksResult').append('<p>' + "The ingredients for yer drinks:" + '</p>' + chooseIngredients);
+    });
 
-  var randomIngredient = function(maximum, minimum) {
-    return (Math.floor(Math.random() * (maximum - minimum + 1)));
-  }
+    var randomIngredient = function(maximum, minimum) {
+        return (Math.floor(Math.random() * (maximum - minimum + 1)));
+    }
 
 });
-  
-  
-
-
-
